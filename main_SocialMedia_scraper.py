aaa673783f496d4fadc439e63452ae4391f819b1
andreireporter13 <andrei.reporter13@gmail.com>
2023-04-04 20:48:22 +0300

    project's files

---
+++ main_SocialMedia_scraper.py
@@ -0,0 +1,48 @@
+#
+#
+#
+# SocialMedia Scraper with Python#(...3...) and Selenium + WebDriver Firefox.
+# This script can scrap facebook groups and can notified you if someone posted something 
+# ... you interested in.
+# ---> This script is for educational purposes!
+# 
+# test facebook group for this is script is ---> https://www.facebook.com/webautomation.romania
+#
+#
+# Author        ---> number_1101
+# Website       ---> https://webautomation.ro
+# Linkedin      ---> https://www.linkedin.com/in/andrei-cojocaru-985932204/
+# Youtube       ---> https://www.youtube.com/channel/UCgx_Y9OHi5KPVzLJo9setxw
+# Facebook      ---> https://www.facebook.com/webautomation.romania
+#
+#
+# python packages
+from time import sleep
+from random import randint
+#
+# my modules
+from browser_settings import configured_driver
+#
+# installed modules: selenium, fake_useragent etc...
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+#
+#
+################################################# START CODE HERE #################################################1
+#
+def func():
+    pass
+
+
+def main():
+    """
+    This function store all logic of this project.
+    Important section!
+    """
+    pass
+
+
+
+# if __name__ == "__main__":
+#   main()
